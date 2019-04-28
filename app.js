25e077361ed029f296d46a12107cbc5d64a673d0
Joshua McGehee <JMcGehee@Joshuas-MacBook-Pro.local>
2019-04-28 15:49:16 -0600

    able to pull latitude and longitude for bike rack locations

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 console.log("LiveGreenOutdoorsChicago");
 
-let map;
-const initMap = () => {
-  map = new google.maps.Map($('#map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+/////////////// -------------- Loading map.
+
+// let map;
+// const initMap = () => {
+//   map = new google.maps.Map($('#map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+// }
+
+////////////////// ------------- Querying locations
+
+//Can I query by location to get lat/long?
+
+//set base query
+const baseUrl = `https://data.cityofchicago.org/resource/cbyb-69xx.json`
+let queryType = `rackid`
+
+//set limit on how many
+const limit = `$limit=`
+let num = `10`
+
+let queryUrl = baseUrl + '?' + '$where=' + queryType + '>2' + '&' + limit + num
+
+
+const getLocations = () => {
+  $.ajax({
+    url: queryUrl
+  }).then((reportData) => {
+    // console.log(reportData);//works
+    for (let i = 0; i < reportData.length; i++) {
+      // console.log(reportData[i].location)//works location is {}
+      $('#map').append($(`<h1>Rack ${i}: ${reportData[i].location.latitude},${reportData[i].location.longitude}</h1>`))
+    }
+  })
 }
 
 $(() => {
-  initMap()
+  /////------------map
+  // initMap()
+
+  /////------location data
+  getLocations();
 })
